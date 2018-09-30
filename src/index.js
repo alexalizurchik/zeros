0a4c36034a1fa83f2a3ea65eeb6beb935f218344
Alexandrina <lizurchik.a@gmail.com>
2018-09-30 19:29:10 +0300

    Add implementation of getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+module.exports = function getZerosCount(num) {
+  let count = 0;
+  
+  while (num >= 5) {
+    num = parseInt(num / 5);
+    count += num;
+  }
+  
+  return count;
 }
